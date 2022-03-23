0d061458f710cd232d8e66284816c5e682b4e904
zilola <example@gmail.com>
2022-03-23 20:57:59 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const answers = [];
+answers[0] = prompt("ismingiz nima?");
+answers[1] = prompt("yoshingiz nechchida?");
+answers[2] = prompt("familiyangiz nima?");
+console.log(answers);
+
+const userName = prompt("ismiz nima");
+
+// alert("hello world");
